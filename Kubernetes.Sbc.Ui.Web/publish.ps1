e1924a7c458c162c10d70a25053d0062932bdbf7
Diego De Medeiros <diegosiao@gmail.com>
2024-07-19 11:20:59 -0300

    New image name convention: branch-commitid-timestamp

--- Kubernetes.Sbc.Ui.Web/publish.ps1
+++ Kubernetes.Sbc.Ui.Web/publish.ps1
@@ -2,7 +2,7 @@ param (
     [string]$tag = "$(Get-Date -Format "yyyyMMddHHmmss")"
 )
 
-$image = "raspberrypi:32000/k8s-sbc-ui-web:local-dev-$($tag)"
+$image = "raspberrypi:32000/k8s-sbc-ui-web:develop-$(git rev-parse --short HEAD)-$($tag)"
 
 dotnet publish --configuration Release --runtime linux-arm64
 if ($LASTEXITCODE -ne 0) { Exit 1 }
